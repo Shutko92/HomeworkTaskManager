02fe841383845a0330d0161b219c0a5523a73096
Shutko <ilya.shutko@gmail.com>
2024-11-27 11:04:00 +0300

    init commit

---
+++ src/test/java/org/homework/taskmanagerservice/TaskManagerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.homework.taskmanagerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
